701913b7487fbfb6c56c9759d22aad6c90f5ed61
Bitard Michaël <bitard.michael@gmail.com>
2019-08-12 21:31:55 +0200

    version 3

--- signal/run.sh
+++ signal/run.sh
@@ -5,4 +5,6 @@
 #NUMBER=$(jq --raw-output ".phone_number" $CONFIG_PATH)
 #
 #/signal-cli-0.6.2/bin/signal-cli --config /data/config/signal -u ${NUMBER} receive -t -1 --json
-env FLASK_APP=app.py flask run --host=0.0.0.0
+export FLASK_APP=app.py
+cd /app
+flask run --host=0.0.0.0
