d0f83a65baf787e023b73d5497c39e034d97ef6b
Bitard Michaël <bitard.michael@gmail.com>
2019-08-14 13:09:49 +0200

    can send messages

--- signal/app.py
+++ signal/app.py
@@ -1,18 +1,45 @@
 from flask import Flask, request
 import os
+import subprocess
+
 app = Flask(__name__)
+
+
+class SignalApplication:
+
+    # def __init__(self):
+    #     self.signal_application_pid = subprocess.Popen(
+    #         SignalApplication.__signal_command(["receive", "-t", "-1", "--json"])).pid
+    #     from time import sleep
+    #     sleep(1)
+    #     print("Process started")
+
+    @staticmethod
+    def __signal_command(command):
+        return [f'/{os.environ["SIGNAL_CLI_PATH"]}/bin/signal-cli',
+                "--config",
+                os.environ["SIGNAL_CONFIG_PATH"],
+                "-u",
+                os.environ["PHONE_NUMBER"],
+                *command]
+
+    def send_message(self, number, message_to_send, attachement=None):
+        my_command = subprocess.run(
+            SignalApplication.__signal_command(['send', '-m', message_to_send, number]), capture_output=True)
+        print(my_command)
+
+
+signal = SignalApplication()
 
 
 @app.route('/message', methods=['POST'])
 def message():
     message_to_send = request.get_json()
-    sendee = message_to_send['number']
+    number = message_to_send['number']
     message_content = message_to_send['content']
-    my_command = os.popen(f'/signal-cli-0.6.2/bin/signal-cli --config /config/.signal -u {os.environ["PHONE_NUMBER"]} send -m "{message_content}" {sendee}').read()
-    print(my_command)
+    signal.send_message(number, message_content)
     return "ok"
 
 
 if __name__ == "__main__":
-    # /signal-cli-0.6.2/bin/signal-cli --config /config/.signal -u ${NUMBER} receive -t -1 --json
     app.run(debug=False)
--- signal/run.sh
+++ signal/run.sh
@@ -3,6 +3,8 @@
 CONFIG_PATH=/data/options.json
 
 export PHONE_NUMBER=$(jq --raw-output ".phone_number" ${CONFIG_PATH})
+export SIGNAL_CLI_PATH=$(jq --raw-output ".signal_cli_path" ${CONFIG_PATH})
+export SIGNAL_CONFIG_PATH=$(jq --raw-output ".signal_config_path" ${CONFIG_PATH})
 
 export FLASK_APP=app.py
 cd /app
